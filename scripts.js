16806d1058ebe16c3befe232cd731caf13e4bdaf
jonathanschneider-sch <jonathan.schneider@escola.pr.gov.br>
2026-09-01 14:28:10 -0300

    ss

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function tocarMusica(nomeMusica) {
+  alert("Reproduzindo prévia de: " + nomeMusica + " - MC Kevin 🎵");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Página carregada e pronta para publicação!");
+});
